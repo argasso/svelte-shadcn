94bfa2b05eced45635cfc2d69401d06dd52ec246
Robert Olsson <robbanolsson@gmail.com>
2024-09-01 21:21:56 +0200

    Without shopify edge function

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,8 @@
 import { HoudiniClient } from '$houdini'
-import { browser } from '$app/environment'
 import { PUBLIC_SHOPIFY_STOREFRONT_TOKEN, PUBLIC_SHOPIFY_STOREFRONT_URL } from '$env/static/public'
 
-const url = browser ? '/shopify' : PUBLIC_SHOPIFY_STOREFRONT_URL
-const headers = browser ? {} : {
+const url = PUBLIC_SHOPIFY_STOREFRONT_URL
+const headers = {
   headers: {
     'X-Shopify-Storefront-Access-Token': PUBLIC_SHOPIFY_STOREFRONT_TOKEN ?? '',
   },
