7d17e7562e96ca7d4cef40d36021c57ee3fc1c10
Robert Olsson <robbanolsson@gmail.com>
2024-09-15 23:23:47 +0200

    fix client

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,20 @@
+import { browser } from '$app/environment'
 import { HoudiniClient } from '$houdini'
 import { PUBLIC_SHOPIFY_STOREFRONT_TOKEN, PUBLIC_SHOPIFY_STOREFRONT_URL } from '$env/static/public'
 
-const url = PUBLIC_SHOPIFY_STOREFRONT_URL
+const url = browser ? '/shopify' : PUBLIC_SHOPIFY_STOREFRONT_URL
 const headers = {
   headers: {
     'X-Shopify-Storefront-Access-Token': PUBLIC_SHOPIFY_STOREFRONT_TOKEN ?? '',
   },
 }
 export default new HoudiniClient({
-  url: '/shopify',
+  url,
   // uncomment this to configure the network call (for things like authentication)
   // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
-  // fetchParams() {
-  //   return {
-  //     ...headers
-  //   }
-  // },
+  fetchParams() {
+    return {
+      ...headers
+    }
+  },
 })
